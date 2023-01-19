164d168c13e5f9e39cbf2494362ee9bf03fb93af
Alexey Maksimov <GeorgieSmetana@gmail.com>
2023-01-19 16:15:30 +0800

    V0.01

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="CSS/style.css">
+    <title>Photographer</title>
+</head>
+<body>
+    <header>
+        <?php
+            require_once "widget/header.php";
+        ?>
+    </header>
+
+    <main>
+
+    </main>
+
+    <footer>
+
+    </footer>
+
+
+    <script src="JS/script.js"></script>
+</body>
+</html>
---
+++ widget/header.php
@@ -0,0 +1,11 @@
+
+    <p>LESHKA LUCHNIK</p>
+
+<div>
+    <div>
+        <a href="#">Портфолио</a>
+        <a href="#">Инфо</a>
+        <a href="#">Контакты</a>
+    </div>
+</div>
+
